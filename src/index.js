3e38a9be8e18f6a1974359ee0ea1cc73408854cc
Hiro OHZATO <hiro@ohzato.com>
2022-05-13 01:18:12 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import "./styles.css";
+
+document.getElementById("app").innerHTML = `
+<h1>Hello Vanilla!</h1>
+<div>
+  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
+</div>
+`;
