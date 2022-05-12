1562a67673eb22ccd82bb266d56642255858899d
Hiro OHZATO <hiro@ohzato.com>
2022-05-13 01:20:03 +0900

    p追加

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import "./styles.css";
 document.getElementById("app").innerHTML = `
 <h1>Hello Vanilla!</h1>
 <div>
+<p>修正</p>
   <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
 </div>
 `;
